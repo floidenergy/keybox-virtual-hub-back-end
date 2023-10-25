f38c904fd0e1b1a74cba265f333cbe3dddada6be
floidenergy <manipulatorrika@gmail.com>
2023-10-25 21:28:01 +0100

    vercel configuration

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const {server, mongoose} = require('./server')
 
+// mongoose.connect(process.env.DB_STRING, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   dbName: "junction"
+// }).then(() => {
+//   console.log("connected to the database");
+//   server.listen(5000, () => console.log('listening on port 5000'));
+// }).catch(err => console.log(err));
+
 mongoose.connect(process.env.DB_STRING, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   dbName: "junction"
-}).then(() => {
-  console.log("connected to the database");
-  server.listen(5000, () => console.log('listening on port 5000'));
-}).catch(err => console.log(err));
+})
+
+module.exports = server
